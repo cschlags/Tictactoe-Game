01cf3c39a524c197bddfcb8fe60db91fdf9ed2a8
ChristinaLeuci <christinaleuci@gmail.com>
2014-03-11 08:43:16 -0400

    Update game.rb

--- game.rb
+++ game.rb
@@ -1,48 +1,39 @@
-#!/usr/bin/env ruby
-
-@slots = {
-  "a1"=>" ","a2"=>" ","a3"=>" ",
-  "b1"=>" ","b2"=>" ","b3"=>" ",
-  "c1"=>" ","c2"=>" ","c3"=>" "
+#all the different slots that you can play
+@slots = { 
+	"a1"=>" ","a2"=>" ","a3"=>" ",
+	"b1"=>" ","b2"=>" ","b3"=>" ",
+	"c1"=>" ","c2"=>" ","c3"=>" "
 }
 
-@winning = [
-  ['a1','a2','a3'],
-  ['b1','b2','b3'],
-  ['c1','c2','c3'],
-  ['a1','b1','c1'],
-  ['a2','b2','c2'],
-  ['a3','b3','c3'],
-  ['a1','b2','c3'],
-  ['c1','b2','a3']
+#all 8 winning matches
+@winning = [ 
+	['a1', 'a2', 'a3'],
+	['b1', 'b2', 'b3'],
+	['c1', 'c2', 'c3'],
+	['a1', 'b1', 'c1'],
+	['a2', 'b2', 'c2'],
+	['a3', 'b3', 'c3'],
+	['a1', 'b2', 'c3'],
+	['c1', 'b2', 'a3']
 ]
 
-#rand selection of x and o between player and comp
-#use .5 since only two options
+# determine who is X or O
+# use .5 because only 2 options [x or o]
+
 @comp = rand() > 0.5 ? 'X' : 'O'
 @player = @comp == 'X' ? 'O' : 'X'
 
-#introductions
-#STDOUT forces immediately out not held
+# player names; introduction
 @comp_name = "Computer"
 puts "TIC TAC TOE"
 puts "Player, what is your name?"
 STDOUT.flush
 @player_name = gets.chomp
 
-if(@player == 'X')
-  puts ""
-  puts "#{@player_name} has the first turn"
-  player_turn
-else
-  puts ""
-  puts "The Computer has the first turn"
-  comp_turn
-end
+#draw game method shows slots, empty and full
+def draw_game
+  puts "-----------------------------------------------------------------------------"
 
-def draw_board
-  puts "============================================================================"
-  
   puts "TIC TAC TOE"
   puts "#{@comp_name}: #{@comp}"
   puts "#{@player_name}: #{@player}"
@@ -54,29 +45,114 @@ def draw_board
   puts " 2 #{@slots["a2"]} | #{@slots["b2"]} | #{@slots["c2"]}"
   puts "   ---------"
   puts " 3 #{@slots["a3"]} | #{@slots["b3"]} | #{@slots["c3"]}"
+  puts ""
+end
+
+def moves_left
+  slots = 0
+  @slots.each do |k, v|
+    slots += 1 if v == " "
+  end
+  slots
+ end
+
+#checking winnings or loses or continuings
+def check_game(next_turn)
+  game_over = false
+  #@winnings.each do |column|
+  for column in [@winnings] do
+    #comp has won?
+    if times_in_column(column, @comp) == 3 #three in a row
+	  puts ""
+      puts "Game Over Dude, the Computer won!!!"
+      game_over = true
+    end
+	
+    #player has won?
+    if times_in_column(column, @player) == 3 #three in a row
+	  puts ""
+      puts "Game Over YOU win!!!"
+      game_over = true
+    end
+  end
+  unless game_over
+    if(moves_left > 0) #no moves left
+      if(next_turn == @player) #from comp_turn method
+        player_turn
+      else
+        comp_turn #from player_turn method
+    end
+	
+    else
+      puts ""
+      puts "Game Over -- DRAW!"
+	  game_over = true
+    end
+  end
 end
 
 def comp_turn
-  move = comp_move
+  move = comp_find_move
   @slots[move] = @comp
   puts ""
   puts "#{@comp_name} marks #{move.upcase}"
-  check_winner(@player)
+  check_game(@player)
 end
 
-def comp_move
-	
-def player_on_board arr, item
+def wrong_move
+  puts ""
+  puts "You must choose an empty slot"
+  player_turn
+end
+
+def wrong_input
+  puts ""
+  puts "Please specify a move with the format 'A3' , 'B1' , 'C2' etc."
+  player_turn
+end
+
+def player_turn
+  puts ""
+  draw_game
+  puts "#{@player_name}, please make a move or type 'exit' to quit"
+  STDOUT.flush
+  input = gets.chomp.downcase
+  if input.length == 2
+    a = input.split("")
+    if(['a','b','c'].include? a[0])
+      if(['1','2','3'].include? a[1])
+        if @slots[input] == " "
+          @slots[input] = @player
+          puts ""
+          puts "#{@player_name} marks #{input.upcase}"
+		  #puts times.times_in_column
+          check_game(@comp)
+        else
+          wrong_move
+        end
+      else
+        wrong_input
+      end
+    else
+      wrong_input
+    end
+  else
+    wrong_input unless input == 'exit'
+  end
+ end
+
+def times_in_column arr, item
   times = 0
-  arr.each do |i|
-    times += 1 if @slots[i] == item
+ #arr.each do |i|
+ for i in [arr] do
+    if @slots[i] != " "
+	  times += 1
     unless @slots[i] == item || @slots[i] == " "
-      #oppisite piece is in column so column cannot be used for win.
-      #therefore, the strategic thing to do is choose a dif column so return 0.
       return 0
     end
+	end
+	return times
   end
-  times
 end
 
 def empty_in_column arr
@@ -87,128 +163,50 @@ def empty_in_column arr
   end
 end
 
-@winning.each do |column|
-  if player_on_board(column, @comp) == 2
+def comp_find_move
+
+#defending against the player first
+for column in [@winnings] do
+  if times_in_column(column, @player) == 2
     return empty_in_column column
   end
 end
 
-@winning.each do |column|
-  if player_on_board(column, @player) == 2
+#choosing best comp choice 2 in column
+for column in [@winnings] do
+  if times_in_column(column, @comp) == 2
     return empty_in_column column
   end
 end
 
-@winning.each do |column|
-  if player_on_board(column, @comp) == 1
+#third best comp choice only one in column
+for column in [@winnings] do
+  if times_in_column(column, @comp) == 1
     return empty_in_column column
   end
 end
 
-#no strategic spot found so just find a random empty
+#choosing an empty slot
 k = @slots.keys;
-i = rand(k.length)
-if @slots[k[i]] == " "
-  return k[i]
+j = rand(k.length)
+if @slots[k[j]] == " "
+  return k[j]
 else
-  #random selection is taken so just find the first empty slot
-  @slots.each { |k,v| return k if v == " " }
+  #first empty slot
+  @slots.each { |k,m| return k if m == " " }
 end
 end
 
-def player_turn
-  puts ""
-  puts " TIC TAC TOE"
-  draw_board
-  puts ""
-  puts " #{@player_name}, please make a move or type 'exit' to quit"
-  puts ""
-  STDOUT.flush
-  input = gets.chomp.downcase
-  
-  #length = 2 since a move is 2 characters a1
-  if input.length == 2
-    a = input.split("")
-    if(['a','b','c'].include? a[0])
-      if(['1','2','3'].include? a[1])
-	  
-	  #if slots empty put player item in
-        if @slots[input] == " "
-          @slots[input] = @player
-		  puts ""
-          puts "#{@player_name} marks #{input.upcase}"
-		  
-		  #check if a winner > next_turn is comp
-          check_winner(@comp)
-        else
-          move_error
-        end
-      else
-        player_error
-      end
-    else
-      player_error
-    end
-  else
-    player_error unless input == 'exit'
-  end
-end
-
-#format error
-def player_error
-  puts ""
-  puts "Please specify a move with the format 'A1' , 'B3' , 'C2' etc."
-  player_turn
-end
-
-#slot error
-def move_error
-  puts ""
-  puts "Please choose an empty space"
-  player_turn
-end
-
-def check_winner(next_turn)
-#nil = false game still goes on
-  game_over = nil
-  
-  #check if an winning groups in column
-  @winning.each do |column|
-    #comp wins
-    if player_on_board(column, @comp) == 3
-      puts ""
-	  puts "Game Over Dude, the Computer won!!!"
-      game_over = true
-    end
-	
-    #player wins
-    if player_on_board(column, @player) == 3
-      puts ""
-	  puts "Oh my gosh!! YOU WON!!!"
-      game_over = true
-    end
-  end
-  
-  unless game_over
-    if(free_moves > 0)
-      if(next_turn == @player)
-        player_turn
-      else
-        comp_turn
-      end
-    else
-	  puts ""
-      puts "Game Over Man it's a DRAW!"
-    end
-  end
-end
 
 
-#the amount of empty spaces left on the board
-def free_moves
-  slots = 0
-  @slots.each do |k, g|
-    slots += 1 if g == " "
-  end
-  slots
+ 
+#actually initiating the methods
+ if(@player == 'X')
+	puts ''
+	puts "#{@player_name} has the first turn"
+	player_turn
+else
+	puts ''
+	puts "#{@comp_name} has the first turn"
+	comp_turn
 end
