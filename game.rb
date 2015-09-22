ebdff06ad734b81dc751e4f9a8c9804794b2a421
Christina Leuci <christinaleuci@gmail.com>
2015-09-22 15:17:13 -0400

    fixed a create player problem

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 #since this class will be the one being run by human require_relative board and player 
 require_relative "./player"
+require_relative "./board"
 
 class Game
   attr_accessor :board, :slots
@@ -11,7 +12,7 @@ class Game
   def create_game
     design_board
     create_players
-    play_move
+    game_play
     # #will need to have defining variables for new players, "x" and "o"?
     # x_player = Player.new("X")
     # o_player = Player.new("O")
@@ -36,8 +37,8 @@ class Game
     puts "Would you like your opponent to be a human or computer? < h / c>"
     response = gets.chomp
     if response == "h"
-      @playerA = Player.new
-      @playerB = Player.new
+      @playerA = Player.new(self)
+      @playerB = Player.new(self)
     elsif response == "c"
       random_start
     else
@@ -51,7 +52,7 @@ class Game
     response = gets.chomp
     if response == "you"
       @playerA = Player.new(self)
-      @playerB = Computer.new(self)
+      @playerB = Player.new(self)
     elsif response == "computer"
       @playerA = Computer.new(self)
       @playerB = Player.new(self)
@@ -63,7 +64,7 @@ class Game
       @playerB = @playerA == Computer.new(self) ? Player.new(self) : Computer.new(self)
     else
       puts "Invalid input"
-      rand_start
+      random_start
     end
   end
 
@@ -85,126 +86,169 @@ class Game
     response == "y" ? Game.new : exit
   end
 
-  def start(x_player, o_player)
-    #player sees board before this
-
-    #first/second is easier. add rand later
-    print "Do you want to play first? <y/n>: "
-    response = gets.chomp
-
-    #if the answer is 'n' then current is 'o_player'
-    #if answer is 'y' then current is 'x_player'
-    if %w[Y y].include?(response)
-      current = x_player
-    else
-      current = o_player
-    end
-
-    #access the play_move method, the first moves. Should happen 9 times because there are nine slots
-    9.times do
-      #which player
-      if current == x_player
-        #player moves
-        play_move(current, board)
-        #switches the players
-        current = o_player
+  def game_play
+    @turn = 0
+    @winner = false
+    while @winner == false && @turn < @slots**2
+      print_board
+      if @turn == (@slots**2)-1
+        puts "It's a tie!"
+        play_again
       else
-        #player moves
-        play_move(current, board)
-        #switches the players
-        current = x_player
+        get_moves
       end
+      @turn += 1
     end
   end
 
-  def stop
-    puts "The Match was a draw"
-    puts ""
-    play_again
-  end
-
-  def play_move(current, board) #the move that each player uses during their turn
-    #is the player a human? eg. "x_player"?
-    if current.symbol == "X"
-      correct = true
-
-      while correct do
-      #if true then ask where they should move using the board numbers
-        print_board
-        puts ""
-        print "Where do want to move? <1-9>: "
-        position = gets.chomp
-        #if the position is wrong or occupied then return to that print
-        if !position.to_i.between?(1, 9)
-          puts "Please use 1..9"
-          correct = true
-        elsif %w[X O].include?board.slots[position]
-          puts "A player chose that spot, please choose an empty space"
-          correct = true
-        else
-          correct = false
-        end
-      end
-
-      #else player is moved using player.move (board, position)?
-      current.move(board, position, self)
-    #if not a human? eg. "o_player"
+  def get_moves
+    puts "Player #{(@turn%2)+1}"
+    if @turn % 2 == 0 
+      @playerA.move("X")
     else
-      current.computer_move(board, self)
-      #then use computer algorithm player.computer_move(board)?
+      @playerB.move("O")
     end
+   check_for_winner
   end
 
-  def check_winner(board) #after each move check if there is a winner
-    #each player should start off with 0 points, each symbol that is part 
-    #of the winning places should gain that player a point.
-    # once a player has 3 points the if should break and moves onto stop 
-    #method to congratulate winner or say draw
-    x_count = 0
-    o_count = 0
-    #go through each board winning slot
-    Board::WINNING.each do |winning_slot|
-      #go through each index of those winning slots (it's a double array)
-      winning_slot.each do |index|
-        #if the board slot has an x
-        if board.slots["#{index}"] == "X"
-          x_count = x_count + 1
-        #if board slot has an o
-        elsif board.slots["#{index}"] == "O"
-          o_count = o_count + 1
-        end
-      end
-      # this should be part of that winning slot .each
-      if x_count == 3 or o_count == 3
-        break
-      else
-        x_count = 0
-        o_count = 0
-      end
-    end
-    #outside of winning slot .each
-    if x_count == 3
-      return "X"
-    elsif o_count == 3
-      return "O"
-    end
-    return "no"
-  end
-
-  def show_winner(winner)
-    puts "Results"
-    if winner == "X"
-      puts "Congratulations! You won!"
-      puts ""
-      play_again
-    else
-      puts "Boo you lost!"
-      puts ""
+  def check_for_winner
+    board = @board.each_slice(slots) {|a| p a}.to_a
+    @tally = 1
+    if vertical_win?(board) || horizontal_win?(board) || diagonal_win?(board)
+      @winner = true
+      print_board
+      puts "Player #{(@turn%2)+1} wins!"
       play_again
     end
-    exit
   end
+
+  def vertical_win?(board)
+    (0...@slots).each do |x|
+      (0...(@slots-1)).each do |y|     
+        @tally += 1 if board[y][x] == board[y+1][x]
+      end
+      if check_tally == true
+        return true
+      end   
+    end
+    false
+  end
+
+  def horizontal_win?(board)
+    (0...@slots).each do |y|
+      (0...(@slots-1)).each do |x|   
+        @tally += 1 if board[y][x] == board[y][x+1]
+      end
+      if check_tally == true
+        return true
+      end
+    end
+    false
+  end
+
+  def diagonal_win?(board)
+    (0...(@slots-1)).each do |i|
+      @tally += 1 if board[i][i] == board[i+1][i+1]
+    end
+    if check_tally == true
+      return true
+    end
+    (0...(@slots-1)).each do |i|
+      @tally += 1 if board[@slots-(i+1)][i] == board[@slots-(i+2)][i+1]
+    end
+    check_tally == true ? true : false
+  end
+
+  def check_tally
+    if @tally == @slots 
+      true
+    else 
+      @tally = 1
+    end 
+  end
+
+  # def play_move(current, board) #the move that each player uses during their turn
+  #   #is the player a human? eg. "x_player"?
+  #   if current.symbol == "X"
+  #     correct = true
+
+  #     while correct do
+  #     #if true then ask where they should move using the board numbers
+  #       print_board
+  #       puts ""
+  #       print "Where do want to move? <1-9>: "
+  #       position = gets.chomp
+  #       #if the position is wrong or occupied then return to that print
+  #       if !position.to_i.between?(1, 9)
+  #         puts "Please use 1..9"
+  #         correct = true
+  #       elsif %w[X O].include?board.slots[position]
+  #         puts "A player chose that spot, please choose an empty space"
+  #         correct = true
+  #       else
+  #         correct = false
+  #       end
+  #     end
+
+  #     #else player is moved using player.move (board, position)?
+  #     current.move(board, position, self)
+  #   #if not a human? eg. "o_player"
+  #   else
+  #     current.computer_move(board, self)
+  #     #then use computer algorithm player.computer_move(board)?
+  #   end
+  # end
+
+#   def check_winner(board) #after each move check if there is a winner
+#     #each player should start off with 0 points, each symbol that is part 
+#     #of the winning places should gain that player a point.
+#     # once a player has 3 points the if should break and moves onto stop 
+#     #method to congratulate winner or say draw
+#     x_count = 0
+#     o_count = 0
+#     #go through each board winning slot
+#     Board::WINNING.each do |winning_slot|
+#       #go through each index of those winning slots (it's a double array)
+#       winning_slot.each do |index|
+#         #if the board slot has an x
+#         if board.slots["#{index}"] == "X"
+#           x_count = x_count + 1
+#         #if board slot has an o
+#         elsif board.slots["#{index}"] == "O"
+#           o_count = o_count + 1
+#         end
+#       end
+#       # this should be part of that winning slot .each
+#       if x_count == 3 or o_count == 3
+#         break
+#       else
+#         x_count = 0
+#         o_count = 0
+#       end
+#     end
+#     #outslots of winning slot .each
+#     if x_count == 3
+#       return "X"
+#     elsif o_count == 3
+#       return "O"
+#     end
+#     return "no"
+#   end
+
+#   def show_winner(winner)
+#     puts "Results"
+#     if winner == "X"
+#       puts "Congratulations! You won!"
+#       puts ""
+#       play_again
+#     else
+#       puts "Boo you lost!"
+#       puts ""
+#       play_again
+#     end
+#     exit
+#   end
+# end
 end
-
 #create the new game
 game = Game.new
