181d6d6afd5073ee02ed386c5ff3f10b296e576c
Christina Leuci <christinaleuci@gmail.com>
2015-09-22 10:12:23 -0400

    added playable slots to board

--- board.rb
+++ board.rb
@@ -2,5 +2,12 @@ class Board
  #creates the board
  #has hash/array of winning places
  
+ #initialize class with the set board positions
+  def initialize # board position starts from 1 to 9
+    @slots = {"1" => " ", "2" => " ", "3" => " ",
+              "4" => " ", "5" => " ", "6" => " ",
+              "7" => " ", "8" => " ", "9" => " "}
+  end
+
 end
 
