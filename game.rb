ee7bbcbca8067cdf9e93abbe919cf05562e760b9
Christina Leuci <christinaleuci@gmail.com>
2015-09-22 14:24:57 -0400

    created a customizably sized board

--- game.rb
+++ game.rb
@@ -2,7 +2,7 @@
 require_relative "./player"
 
 class Game
-
+  attr_accessor :board, :slots
   def initialize
     play
   end
@@ -11,9 +11,11 @@ class Game
     #will need to have defining variables for new players, "x" and "o"?
     x_player = Player.new("X")
     o_player = Player.new("O")
+    board = design_board
     #game needs to have a start and stop method for if someone wins
-    game.start(x_player, o_player, board) #should start the game immediately being the main method displaying and user first experience of player choosing
-    game.stop #put back in to have a draw
+    print_board
+    start(x_player, o_player, print_board) #should start the game immediately being the main method displaying and user first experience of player choosing
+    stop #put back in to have a draw
   end
 
   def play_again
@@ -22,6 +24,27 @@ class Game
     again == "y" ? Game.new : exit
   end
 
+  def design_board
+    puts "How big would you like the board to be? < 3 / 4 / 5 >"
+    @slots = gets.to_i
+    if @slots <= 0 || @slots > 5
+      puts "That is not valid."
+      design_board
+    end    
+    @board = (1..@slots**2).to_a
+  end
+
+  def print_board
+    board = @board.each_slice(slots) {|a| p a}.to_a
+    printed_board = "\n"
+    board.each_with_index do |row, i|
+      row.each do |char|
+        printed_board += char.to_s.rjust(@slots**2.to_s.length, " ")
+      end
+      printed_board += "\n\n" 
+    end
+    puts printed_board
+  end
   def start(x_player, o_player, board)
     #player sees board before this
 
@@ -60,36 +83,36 @@ class Game
     play_again
   end
 
-  def play(current, board) #the move that each player uses during their turn
-    #is the player a human? eg. "x_player"?
-    if current.symbol == "X"
-      correct = true
+  # def play(current, board) #the move that each player uses during their turn
+  #   #is the player a human? eg. "x_player"?
+  #   if current.symbol == "X"
+  #     correct = true
 
-      while correct do
-      #if true then ask where they should move using the board numbers
-        puts ""
-        print "Where do want to move? <1-9>: "
-        position = gets.chomp
-        #if the position is wrong or occupied then return to that print
-        if !position.to_i.between?(1, 9)
-          puts "Please use 1..9"
-          correct = true
-        elsif %w[X O].include?board.slots[position]
-          puts "A player chose that spot, please choose an empty space"
-          correct = true
-        else
-          correct = false
-        end
-      end
+  #     while correct do
+  #     #if true then ask where they should move using the board numbers
+  #       puts ""
+  #       print "Where do want to move? <1-9>: "
+  #       position = gets.chomp
+  #       #if the position is wrong or occupied then return to that print
+  #       if !position.to_i.between?(1, 9)
+  #         puts "Please use 1..9"
+  #         correct = true
+  #       elsif %w[X O].include?board.slots[position]
+  #         puts "A player chose that spot, please choose an empty space"
+  #         correct = true
+  #       else
+  #         correct = false
+  #       end
+  #     end
 
-      #else player is moved using player.move (board, position)?
-      current.move(board, position, self)
-    #if not a human? eg. "o_player"
-    else
-      current.computer_move(board, self)
-      #then use computer algorithm player.computer_move(board)?
-    end
-  end
+  #     #else player is moved using player.move (board, position)?
+  #     current.move(board, position, self)
+  #   #if not a human? eg. "o_player"
+  #   else
+  #     current.computer_move(board, self)
+  #     #then use computer algorithm player.computer_move(board)?
+  #   end
+  # end
 
   def check_winner(board) #after each move check if there is a winner
     #each player should start off with 0 points, each symbol that is part 
